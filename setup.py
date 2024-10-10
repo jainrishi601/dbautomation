9a339d019b50740af5545ed0317f1b22b4c0414f
Rishi Jain <jainrishi0311@gmail.com>
2024-10-11 01:41:12 +0530

    rishi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
    
 
 __version__ = "0.0.4"
-REPO_NAME = "mongodbconnectorpkg"
+REPO_NAME = "dbautomation"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "jainrishi601"
 AUTHOR_EMAIL = "jainrishi0311@gmail.com"
